2b003b540ca5f53ed9a7d1f0dd170b5b5115b885
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-03-22 19:50:14 -0400

    Basic Card Layout Made

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
+
 
 class Project extends Component {
     render() {
+        console.log(this.props)
+        let {name, description, technologiesUsed, frontGitHubLink, backGitHubLink, liveDemo, videoDemo } = this.props.project 
         return (
-            <div>
-                Ho ho ho 
+            <div style = {{textAlign: 'center'}}>
+                <div style ={{width: '80%', marginLeft: 'auto', marginRight: 'auto'}}>
+                    <Card fluid className ='projectCard'>
+                        <Image src='https://media1.tenor.com/images/642302f2c212571f2b82128bd961c34e/tenor.gif?itemid=9978863' wrapped ui={false} />
+                        <Card.Content>
+                        <Card.Header>{name}</Card.Header>
+                        <Card.Meta>
+                            <span className='date'>{description}</span>
+                        </Card.Meta>
+                        <Card.Description>
+                            <h4>Technologies Used:</h4> {technologiesUsed.join(', ')}
+                        </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                        {frontGitHubLink ? <a href = {frontGitHubLink} rel="noopener noreferrer" target = '_blank'>
+                            Frontend Repo
+                        </a>: null} {' '} {' '}
+                        {backGitHubLink ? <a href = {backGitHubLink}  rel="noopener noreferrer" target = '_blank'>
+                            Backend Repo
+                        </a>:null} {' '}{' '}
+                        {liveDemo ? <a href = {liveDemo} rel="noopener noreferrer" target = '_blank'>
+                            Live Demo
+                        </a>:null}{' '}{' '}
+                        {videoDemo ? <a href = {videoDemo} target = '_blank'>
+                            Video Demo 
+                        </a>:null}
+                        </Card.Content>
+                    </Card>
+                </div>
             </div>
         )
     }
--- src/Components/Projects/ProjectsContainer.jsx
+++ src/Components/Projects/ProjectsContainer.jsx
@@ -5,14 +5,23 @@ import Project from './Project'
 class ProjectsContainer extends React.Component{
 
     state = {
-        projects: [ {id: 1}, {id:2}]
+        projects: [ 
+            {id: 1, 
+            name: 'The Youniverse',
+            description: 'This is a space-themed social media application centered around promoting emotional intelligence.',
+            technologiesUsed: ['React', 'Redux', 'Semantic UI React', 'Websockets/ActionCable', 'Ruby on Rails', 'JWT Token Authentication'],
+            frontGitHubLink: 'https://github.com/jonathanbrierre/TheYouniverse-Front',
+            backGitHubLink: 'https://github.com/jonathanbrierre/TheYouniverse-Back',
+            liveDemo: null,
+            videoDemo: 'https://www.youtube.com/watch?v=93rDLcB3dKI&t=5s'
+            }
+        ]
     }
 
     renderProjects = () => {
-       return this.state.projects.map(project => <Project key = {project.id} project = {project}/>)
+        return this.state.projects.map(project => <Project key = {project.id} project = {project}/>)
     }
 
-    
 
     render(){
         return (
