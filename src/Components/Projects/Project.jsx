0d0bc182360aa6e968edce5c06487933588f4e4a
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-03-22 19:00:33 -0400

    set up proj container for seeds

---
+++ src/Components/Projects/Project.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Project extends Component {
+    render() {
+        return (
+            <div>
+                Ho ho ho 
+            </div>
+        )
+    }
+}
+
+export default Project
--- src/Components/Projects/ProjectsContainer.jsx
+++ src/Components/Projects/ProjectsContainer.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import NavBar from '../NavBar'
+import Project from './Project'
 
-const ProjectsContainer = () => {
+class ProjectsContainer extends React.Component{
 
-    return (
-        <div>
-            <NavBar/>
-            Hello from the projects
-        </div>
-    )
+    state = {
+        projects: [ {id: 1}, {id:2}]
+    }
+
+    renderProjects = () => {
+       return this.state.projects.map(project => <Project key = {project.id} project = {project}/>)
+    }
+
+    
+
+    render(){
+        return (
+            <div>
+                <NavBar/>
+                {this.renderProjects()}
+            </div>
+    )}
 }
 
 export default ProjectsContainer
