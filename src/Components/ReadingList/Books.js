d4203ca9f6cb140625f6174375138bb5d0cf179e
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-05-22 17:08:13 -0400

    added under construction sign & book.js file

---
+++ src/Components/ReadingList/Books.js
@@ -0,0 +1,17 @@
+
+
+const bookList = [
+    {
+        title: 'King, Warrior, Magician, Lover',
+        author: ' Robert Moore & Douglas Gillette'
+    },
+    {
+        title: 'The Way of The Superior Man',
+        author: 'David Deida'
+    },
+    {
+        title: 'How To Win Friends and Influence People In The Digital Age',
+        author: 'Dale Carnegie & Associates'
+    }
+]
+export default bookList
--- src/Components/ReadingList/ReadingList.jsx
+++ src/Components/ReadingList/ReadingList.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import bookList from './Books'
+import Book from './Book'
+import NavBar from '../NavBar'
 
-export class ReadingList extends Component {
+class ReadingList extends Component {
+
+
+    
     render() {
         return (
             <div>
-                
+                <NavBar/>
+                <div className = 'contentContainer'>
+                Under Construction
+                {bookList.map(book => <Book book = {book}/>)}
+
+                </div>
             </div>
         )
     }
