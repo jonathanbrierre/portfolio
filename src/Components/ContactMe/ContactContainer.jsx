5a8a4c5765a5e28f2521e2561b8b43c85812a022
Jonathan <jonathanbrierre@yahoo.com>
2022-09-22 14:34:03 -0400

    book to e-book

--- src/Components/ContactMe/ContactContainer.jsx
+++ src/Components/ContactMe/ContactContainer.jsx
@@ -12,7 +12,7 @@ class Contact extends Component {
                     <br></br>
                     <a href = 'https://calendly.com/jonathanbrierre/one-on-one'  rel="noopener noreferrer" target= '_blank'> <h3>Book a call</h3></a>
                     <br></br>
-                    <a href = 'https://www.amazon.com/Jonathan-Brierre/e/B07YHVJ6X3?ref_=dbs_p_ebk_r00_abau_000000' rel="noopener noreferrer" target = '_blank'><h3>Buy my book for a dollar</h3></a>
+                    <a href = 'https://www.amazon.com/Jonathan-Brierre/e/B07YHVJ6X3?ref_=dbs_p_ebk_r00_abau_000000' rel="noopener noreferrer" target = '_blank'><h3>Buy my e-book for a dollar</h3></a>
                     <br></br>
                     <a href = 'https://dot.cards/jonathanbrierre?a=user%2Fjxdazichkhxiqtwcef3mifi6a%2Fc%2Fbl' rel="noopener noreferrer" target = '_blank'><h3>Business card</h3></a>
                     <br></br>
