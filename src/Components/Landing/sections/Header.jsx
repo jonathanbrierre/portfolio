e2cfd8e8c861cbb93418622204ad07394d048f9a
Jonathan <jonathanbrierre@yahoo.com>
2023-05-06 19:50:57 -0400

    --amend

--- src/Components/Landing/sections/Header.jsx
+++ src/Components/Landing/sections/Header.jsx
@@ -1,32 +1,8 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 
-function Header({ setTurnBurgerWhite }) {
-
-    const observerRef = useRef(null)
-
-    useEffect(() => {
-        const headerObserver = new IntersectionObserver(
-            function (entries) {
-                if (entries[0].isIntersecting === true) {
-                    setTurnBurgerWhite(true)
-                } else {
-                    setTurnBurgerWhite(false)
-                }
-            },
-            { threshold: [0.9] }
-        );
-        if (observerRef.current) {
-            headerObserver.observe(observerRef.current)
-        }
-
-        return () => {
-            headerObserver.disconnect()
-        }
-    }, [observerRef, setTurnBurgerWhite])
-
+function Header() {
     return (
         <div className='header-container' >
-
             <div className="text-container">
                 <div className='my-name'>
                     <h1>Jonathan <br /> Brierre</h1>
@@ -46,7 +22,6 @@ function Header({ setTurnBurgerWhite }) {
                     <a href="https://calendly.com/jonathanbrierre/one-on-one" target="_blank" rel='noopener noreferrer'> <button> BOOK ME</button></a>
                 </div>
             </div>
-            <div ref={observerRef} className="bottom-observer"></div>
         </div>
     )
 }
