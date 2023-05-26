973d3970fe99cf454eb6e5e228a68d843a027bd4
Jonathan <jonathanbrierre@yahoo.com>
2023-05-25 21:04:46 -0400

    changed press coordinator to public relations

--- src/Components/Landing/sections/Bio.jsx
+++ src/Components/Landing/sections/Bio.jsx
@@ -9,7 +9,7 @@ function Bio() {
             <div className='bio-text'>
                 <h2>Bio</h2>
                 <p>
-                    Jonathan Brierre is an entrepreneur, author, transformational coach, and public speaker who has dedicated his life to helping others achieve their goals and find inner peace. Brierre's unique philosophy on happiness and success is grounded in the belief that true fulfillment comes from living a life of purpose and serving others. He is the writer of the Amazon bestseller "A Short Handbook For Happiness," and has worked with many clients as a coach, mentor, workshop facilitator, and press coordinator.
+                    Jonathan Brierre is an entrepreneur, author, transformational coach, and public speaker who has dedicated his life to helping others achieve their goals and find inner peace. Brierre's unique philosophy on happiness and success is grounded in the belief that true fulfillment comes from living a life of purpose and serving others. He is the writer of the Amazon bestseller "A Short Handbook For Happiness," and has worked with many clients as a coach, mentor, workshop facilitator, and public relations coordinator.
                 </p>
             </div>
         </div>
